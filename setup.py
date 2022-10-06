a9b0466fef81ff3173d986131cd7618f7988c602
Kushal Das <mail@kushaldas.in>
2022-10-06 17:08:49 +0200

    Updates for 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="pyarti",
-    version="0.0.1",
+    version="0.0.2",
     rust_extensions=[RustExtension("pyarti.pyarti", binding=Binding.PyO3)],
     package_data={"pyarti": ["py.typed", "pyarti.pyi"]},
     packages=["pyarti"],
